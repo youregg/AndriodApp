eb1a8d512ba4ded95aa596b4e678fa3fc636723e
lanyetc <2216446158@qq.com>
2018-03-24 20:57:52 +0800

    initialize with gitignore

---
+++ app/src/main/java/com/example/lanyetc/campusgo/LoginActivity.java
@@ -0,0 +1,177 @@
+package com.example.lanyetc.campusgo;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+import android.view.Gravity;
+
+public class LoginActivity extends AppCompatActivity implements OnClickListener {
+
+    // 登陆按钮
+    private ImageButton logbtn;
+    // 调试文本，注册文本
+    private TextView infotv, regtv;
+    // 显示用户名和密码
+    EditText username, password;
+    // 创建等待框
+    private ProgressDialog dialog;
+    // 返回的数据
+    private String info;
+    // 返回主线程更新数据
+    private static Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        if(Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            //让应用主题内容占用系统状态栏的空间,注意:下面两个参数必须一起使用 stable 牢固的
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            //设置状态栏颜色为透明
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        // 获取控件
+        username = (EditText) findViewById(R.id.login_uername);
+        password = (EditText) findViewById(R.id.login_password);
+        logbtn = (ImageButton) findViewById(R.id.login_btn);
+        infotv = (TextView) findViewById(R.id.info);
+
+        // 设置按钮监听器
+        logbtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            //点击登陆anniu
+            case  R.id.login_btn:
+                //检测网络
+                if (!checkNetwork(this)) {
+                    Toast toast = Toast.makeText(LoginActivity.this,"网络未连接", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    break;
+                }
+
+                //提示框
+                dialog = new ProgressDialog(this);
+                dialog.setTitle("提示");
+                dialog.setMessage("正在登陆，请稍后...");
+                dialog.setCancelable(false);
+                dialog.show();
+                // 创建子线程，分别进行Get和Post传输
+                new Thread(new MyThread()).start();
+                break;
+
+            //点击现在注册按钮
+            case R.id.login_sign_btn:
+                Intent regItn = new Intent(LoginActivity.this, SignActivity.class); //启动注册Activity
+                // overridePendingTransition(anim_enter);
+                startActivity(regItn);
+                break;
+
+        }
+    }
+
+    // 子线程接收数据，主线程修改数据
+    public class MyThread implements Runnable {
+        @Override
+        public void run() {
+            info = WebService.executeHttpGet(username.getText().toString(), password.getText().toString());
+            // info = WebServicePost.executeHttpPost(username.getText().toString(), password.getText().toString());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    infotv.setText(info);
+                    dialog.dismiss();
+                }
+            });
+        }
+    }
+
+    // 检测网络
+    private boolean checkNetwork(Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo info = connectivity.getActiveNetworkInfo();
+            if (info != null && info.isConnected())
+            {
+                // 当前网络是连接的
+                if (info.getState() == NetworkInfo.State.CONNECTED)
+                {
+                    // 当前所连接的网络可用
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //点击空白处输入框自动失焦并收起输入法
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideInput(v, ev)) {
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
+    }
+
+    public boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = {0, 0};
+            //获取输入框当前的location位置
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击的是输入框区域，保留点击EditText的事件
+                return false;
+            } else {
+                //使EditText触发一次失去焦点事件
+                v.setFocusable(false);
+//                v.setFocusable(true); //这里不需要是因为下面一句代码会同时实现这个功能
+                v.setFocusableInTouchMode(true);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
